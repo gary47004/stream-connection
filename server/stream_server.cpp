07b9307aea0843521f53ab9552c15303dcac0ad9
gary <gary47004@gmail.com>
2024-03-12 21:53:50 +0800

    FEATURE: server argv (#4)

--- server/stream_server.cpp
+++ server/stream_server.cpp
@@ -4,6 +4,7 @@
 #include <grpcpp/server_builder.h>
 #include <grpcpp/server_context.h>
 
+#include <functional>
 #include <iostream>
 #include <memory>
 #include <string>
@@ -25,15 +26,15 @@ using streamconnection::StreamConnection;
 
 class StreamConnectionImpl final : public StreamConnection::Service {
  public:
-  StreamConnectionImpl() {}
+  StreamConnectionImpl(std::function<std::string()> generate_resp_msg)
+      : generate_resp_msg_(generate_resp_msg) {}
 
   Status GetValues(ServerContext* context,
                    ServerReaderWriter<Response, Request>* stream) override {
     Request req;
     while (stream->Read(&req)) {
       Response res;
-      count_++;
-      res.set_value(std::to_string(count_));
+      res.set_value(generate_resp_msg_());
       stream->Write(res);
     }
 
@@ -41,12 +42,23 @@ class StreamConnectionImpl final : public StreamConnection::Service {
   }
 
  private:
-  int count_ = 0;
+  std::function<std::string()> generate_resp_msg_;
 };
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cout << "One and only argument [server name] is needed." << std::endl;
+    return 0;
+  }
+  std::string server_name = argv[1];
+  int count = 0;
+  auto generate_resp_msg = [&count, &server_name]() -> std::string {
+    return "Server name: " + server_name +
+           "\nMessage count: " + std::to_string(++count);
+  };
+
   std::string server_address("0.0.0.0:50051");
-  StreamConnectionImpl service;
+  StreamConnectionImpl service(generate_resp_msg);
 
   ServerBuilder builder;
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
